0fdbdbc23a4e805cd30a100cd60dab3327fef5db
kikitheguillotine <175551755+kikitheguillotine@users.noreply.github.com>
2026-04-19 10:28:52 -0300

    feat: implement function to update billing status in json

--- json_handling.py
+++ json_handling.py
@@ -1,26 +1,40 @@
 import json
 
 
-def data_variable_setting():
-    with open("data.json", "r", encoding="utf8") as file:
-        data = json.load(file)
-    return data
+def load_data():
+    with open("contract_data.json", "r", encoding="utf8") as file:
+        billing_data = json.load(file)
+    return billing_data
 
 
-def ask_milestone_to_be_billed(data):
-    for i, milestone in data["paymentSchedule"].items():
-        print("[" + i + "]")
-        for key, description in milestone.items():
+def select_milestone(billing_data):
+    for milestone_id, milestone in billing_data["paymentSchedule"].items():
+        print("[" + milestone_id + "]")
+        for key, value in milestone.items():
             print(
                 key + ":",
-                description,
+                value,
             )
         print()
-    tobe_billed = input("Enter code for the milestone to be billed: ")
-    return tobe_billed
+    selected_milestone = input("Enter code for the milestone to be billed: ")
+    return selected_milestone
 
 
-data = data_variable_setting()
+def update_billing_data(billing_data, selected_milestone):
+    billing_data["paymentSchedule"][selected_milestone]["billed"] = True
+    return billing_data
 
-tobe_billed = ask_milestone_to_be_billed(data)
-print(tobe_billed)
+
+def update_contract_data(billing_data):
+    with open("contract_data.json", "w", encoding="utf8") as file:
+        json.dump(billing_data, file, indent=2)
+
+
+billing_data = load_data()
+print(billing_data)
+
+selected_milestone = select_milestone(billing_data)
+
+billing_data = update_billing_data(billing_data, selected_milestone)
+print(billing_data)
+update_contract_data(billing_data)
